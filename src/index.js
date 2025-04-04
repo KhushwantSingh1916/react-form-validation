2cf85405d473ba73925c2236dc41d5b0742557a3
khushwant singh <khushwantsingh1916@gmail.com>
2025-04-05 01:22:37 +0530

    css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RegistrationForm from './form.jsx';
 import './index.css';
+import RegistrationForm from './form.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
